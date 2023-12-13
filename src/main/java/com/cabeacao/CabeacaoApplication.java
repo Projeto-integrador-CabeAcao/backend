11da33526298c538dfe4383548a7bd70bd45d9ce
Kiev <kevinkiss2010@gmail.com>
2023-12-13 12:03:01 -0300

    Criando o Projeto Cabe Ação

---
+++ src/main/java/com/cabeacao/CabeacaoApplication.java
@@ -0,0 +1,13 @@
+package com.cabeacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabeacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabeacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cabeacao/CabeacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabeacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabeacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
